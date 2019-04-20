93d7e00281d2f9ac3bac76e0ee16c3149214290d
hqduan <hqduan@thoughtworks.com>
2019-04-20 21:54:32 +0800

    add multiple sound files

--- App.js
+++ App.js
@@ -7,11 +7,12 @@
  */
 
 import React, { Component } from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import AudioComponent from './audio';
+import DrawerLayout from 'react-native-drawer-layout';
 
 type Props = {};
-const audios = ['rainy', 'coffee'];
+const audios = ['rainy', 'coffee', 'fire', 'thunder', 'wind', 'sea'];
 export default class App extends Component<Props> {
 
   state = {
@@ -19,34 +20,45 @@ export default class App extends Component<Props> {
   };
 
   render() {
-    return (
-      <View style={{
-        display: 'flex',
-        height: 100,
-        padding: 20
-      }}>
-        {
-          audios.map(audioFileName =>
-            <AudioComponent fileName={audioFileName}
-                            key={audioFileName}
-                            isPlay={this.state.isPlay} />
-          )
-        }
-        <View>
-          <Button
-            onPress={this.onPlayButton}
-            title="PLAY"
-            color="#841584"
-          />
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <Button
-            onPress={this.onStopButton}
-            title="STOP"
-            color="#841584"
-          />
-        </View>
+    const navigationView = (
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <Text style={{ margin: 10, fontSize: 15, textAlign: 'left' }}>I'm in the Drawer!</Text>
       </View>
+    );
+
+    return (
+      <DrawerLayout
+        drawerWidth={300}
+        drawerPosition={DrawerLayout.positions.Left}
+        renderNavigationView={() => navigationView}>
+        <View style={{
+          display: 'flex',
+          height: 100,
+          padding: 20
+        }}>
+          {
+            audios.map(audioFileName =>
+              <AudioComponent fileName={audioFileName}
+                              key={audioFileName}
+                              isPlay={this.state.isPlay} />
+            )
+          }
+          <View>
+            <Button
+              onPress={this.onPlayButton}
+              title="PLAY"
+              color="#841584"
+            />
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Button
+              onPress={this.onStopButton}
+              title="STOP"
+              color="#841584"
+            />
+          </View>
+        </View>
+      </DrawerLayout>
     );
   }
 
